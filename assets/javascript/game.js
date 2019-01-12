167712cde509cd0044b714c5c097cb7f460b9342
Mo <moh.alneama@yahoo.com>
2019-01-12 16:54:52 -0600

    adding a clickHandler

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,22 @@ dNum3  = Math.floor(Math.random()*(maxD-minD))+ minD;  // 1 - 12
 dNum4  = Math.floor(Math.random()*(maxD-minD))+ minD;  // 1 - 12
 
 
+$("#score").text(randonNumber);
+$("#btn1").val(dNum1);
+$("#btn2").val(dNum2);
+$("#btn3").val(dNum3);
+$("#btn4").val(dNum4);
 
+function clickHandler(){
+    let btnVal = parseInt($(this).val());
+    total += btnVal;
+    $("#total").text(total);
+}
+
+$("#btn1").click(clickHandler);
+$("#btn2").click(clickHandler);
+$("#btn3").click(clickHandler);
+$("#btn4").click(clickHandler);
+     
     
 }
