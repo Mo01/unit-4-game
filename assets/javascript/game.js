99be4612cc1bf81cecd9abbb286a68d67ecd6e4e
Mo <moh.alneama@yahoo.com>
2019-01-12 16:39:46 -0600

    generate random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,18 @@
 window.onload = function what() {
 //generate random number
-var minNumber = 39;
-var maxNumber = 99;
-var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+var minNum = 19;
+var maxNum = 121;
+var minD = 1;
+var maxD = 13;
+var total = 0;
 
-function randomNumberFromRange(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-console.log(randomNumber);
-document.getElementById("score").innerHTML = randomNumber;
+randonNumber =  Math.floor(Math.random()*(maxNum-minNum))+ minNum;  // 19 - 120
+dNum1  = Math.floor(Math.random()*(maxD-minD))+ minD;  // 1 - 12
+dNum2  = Math.floor(Math.random()*(maxD-minD))+ minD;  // 1 - 12
+dNum3  = Math.floor(Math.random()*(maxD-minD))+ minD;  // 1 - 12
+dNum4  = Math.floor(Math.random()*(maxD-minD))+ minD;  // 1 - 12
 
 
+
+    
 }
