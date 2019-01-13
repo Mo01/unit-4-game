0becc38c8f9196ff62f98cf695d4db6563493706
Mo <moh.alneama@yahoo.com>
2019-01-12 18:12:17 -0600

    Clean up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,26 +7,42 @@ window.onload = function what() {
     var total = 0;
     var win = 0;
     var losse = 0;
-    var randonNumber = Math.floor(Math.random() * (maxNum - minNum)) + minNum;  // 19 - 120
 
+    //Generate random numbers
+    var randonNumber = Math.floor(Math.random() * (maxNum - minNum)) + minNum;  // 19 - 120
+    $("#score").text(randonNumber);
+
+    //Runs every click
     var clickHandler = function () {
         let btnVal = parseInt($(this).val());
         total += btnVal;
         $("#total").text(total);
         if (total === randonNumber) {
-            console.log("You Won!!");
+            randonNumber = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+            total = 0;
+            win++;
+            $("#wins").text("Wins = " + win);
+            reset();
         }
         else if (total > randonNumber) {
-            console.log("You lost!!");
+            randonNumber = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+            total = 0;
+            losse++;
+            $("#losses").text("Losses = " + losse);
+            reset();
         }
     }
 
-    $("#score").text(randonNumber);
-    [$("#btn1"), $("#btn2"), $("#btn3"), $("#btn4")].forEach(function (btn) {
-        var val = Math.floor(Math.random() * (maxD - minD)) + minD;
-        btn.val(val);
-        btn.click(clickHandler);
-    })
+    //Reset function
+    function reset() {
+        [$("#btn1"), $("#btn2"), $("#btn3"), $("#btn4")].forEach(function (btn) {
+            var val = Math.floor(Math.random() * (maxD - minD)) + minD;
+            btn.val(val);
+            btn.off('click').on('click', clickHandler);
+            $("#total").text(total);
+            $("#score").text(randonNumber);
+        })
 
-
+    }
+    reset();
 }
